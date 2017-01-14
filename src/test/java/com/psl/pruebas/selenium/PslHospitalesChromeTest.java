283984eac91f4dfe9c31d1511cd371d9fc29335d
Miguel Arcos <miguelarcos20@gmail.com>
2017-01-14 00:24:39 -0500

    Entrega final

--- src/test/java/com/psl/pruebas/selenium/PslHospitalesChromeTest.java
+++ src/test/java/com/psl/pruebas/selenium/PslHospitalesChromeTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 import java.io.IOException;
@@ -64,6 +65,49 @@ public class PslHospitalesChromeTest {
         driver.findElement(By.linkText("Guardar")).click();
 
         assertEquals( "miguel",driver.findElement(By.id("firstName")).getText() );
+
+    }
+
+    @Test
+    public void validateAddPaciente() throws IOException {
+        driver.findElement(By.linkText("Agregar Paciente")).click();
+        driver.findElement(By.name("name")).clear();
+        driver.findElement(By.name("name")).sendKeys("Lauriano");
+        driver.findElement(By.name("last_name")).clear();
+        driver.findElement(By.name("last_name")).sendKeys("Marcos");
+        driver.findElement(By.name("telephone")).clear();
+        driver.findElement(By.name("telephone")).sendKeys("09855");
+        driver.findElement(By.name("identification")).clear();
+        driver.findElement(By.name("identification")).sendKeys("097777");
+        driver.findElement(By.name("prepaid")).click();
+        driver.findElement(By.linkText("Guardar")).click();
+
+        assertEquals( "Lauriano",driver.findElement(By.className("patientName")).getText() );
+
+    }
+
+    @Test
+    public void validateAddHospitalAndConsult() throws IOException {
+        driver.findElement(By.linkText("Agregar Hospital")).click();
+        driver.findElement(By.cssSelector("input.form-control")).clear();
+        driver.findElement(By.cssSelector("input.form-control")).sendKeys("12345");
+        driver.findElement(By.xpath("(//input[@type='text'])[2]")).clear();
+        driver.findElement(By.xpath("(//input[@type='text'])[2]")).sendKeys("Hospital central");
+        driver.findElement(By.xpath("(//input[@type='text'])[3]")).clear();
+        driver.findElement(By.xpath("(//input[@type='text'])[3]")).sendKeys("11111");
+        driver.findElement(By.xpath("(//input[@type='text'])[4]")).clear();
+        driver.findElement(By.xpath("(//input[@type='text'])[4]")).sendKeys("3333");
+        new Select(driver.findElement(By.id("department"))).selectByVisibleText("Antioquia");
+        new Select(driver.findElement(By.id("municipality"))).selectByVisibleText("Abejorral");
+        driver.findElement(By.linkText("Guardar")).click();
+
+        assertEquals( "Código",driver.findElement(By.cssSelector(".table-striped > tbody > tr:nth-child(2n+1) > td, .table-striped > tbody > tr:nth-child(2n+1) > th")).getText() );
+
+
+       driver.findElement(By.linkText("Inicio")).click();
+        driver.findElement(By.linkText("Ver Hospitales")).click();
+
+        assertEquals( "12345",driver.findElement(By.cssSelector(".table-striped > tbody > tr:nth-child(2n+1) > td, .table-striped > tbody > tr:nth-child(2n+1) >th")).getText() );
 
     }
 
